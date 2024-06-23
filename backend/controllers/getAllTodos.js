fdfc2c825f6189a6a21f6b7335286b04d61ac8c5
jyotiraditya <jyotiraditya24@gmail.com>
2024-06-23 22:23:16 +0530

    todo CRUD operation done

--- backend/controllers/getAllTodos.js
+++ backend/controllers/getAllTodos.js
@@ -38,7 +38,54 @@ export const createTodos = async (req, resp) => {
     }
 
     // Respond with the saved todo, excluding the 'createdBy' field
-    return resp.json(savedSingleTodo.select("-createdBy"));
+    return resp.status(201).json(savedSingleTodo);
+  } catch (error) {
+    console.error(error.message);
+    return resp.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const deleteTodos = async (req, resp) => {
+  const { id: todoId } = req.params;
+  try {
+    if (!todoId) {
+      return resp.status(400).json({ error: "All fields are not present" });
+    }
+    const deletedTodo = await ToDo.findByIdAndDelete(todoId);
+    if (!deletedTodo) {
+      return resp.status(404).json({ error: "Todo not found" });
+    }
+    return resp.json({
+      message: "Todo deleted successfully",
+      deletedTodo: deletedTodo,
+    });
+  } catch (error) {
+    console.error(error.message);
+    return resp.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const updateTodos = async (req, resp) => {
+  const { header, content } = req.body;
+  const { id: todoId } = req.params;
+  try {
+    const todo = await ToDo.findById(todoId);
+
+    if (!todo || todo.length === 0) {
+      return resp.status(400).json({ error: "No todos present by the user" });
+    }
+
+    const updatedTodo = await ToDo.findByIdAndUpdate(
+      todoId,
+      { header, content },
+      { new: true } // returns the updated document
+    );
+
+    if (!updatedTodo) {
+      return resp.status(400).json({ error: "Could not update Todo" });
+    }
+
+    return resp.json(updatedTodo);
   } catch (error) {
     console.error(error.message);
     return resp.status(500).json({ error: "Internal Server Error" });
--- backend/routes/ToDoRoutes.js
+++ backend/routes/ToDoRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { getAllTodos } from "../controllers/getAllTodos.js";
+import {
+  createTodos,
+  getAllTodos,
+  deleteTodos,
+  updateTodos,
+} from "../controllers/getAllTodos.js";
 
 const router = express.Router();
 
 router.get("/getAllTodos/:id", getAllTodos);
-
+router.post("/createTodos/:id", createTodos);
+router.delete("/deleteTodos/:id", deleteTodos);
+router.patch("/updateTodos/:id", updateTodos); //each todo id
+ 
 export default router;
