dac5c7a764c2fb41359a6bf59e9359d9301c0f06
jyotiraditya <jyotiraditya24@gmail.com>
2024-06-25 01:47:05 +0530

    Can we deploy it now?

--- server/index.js
+++ server/index.js
@@ -5,12 +5,16 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import toDoRoutes from "./routes/ToDoRoutes.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
-const __dirname = path.resolve();
+// Resolving dirname for ES Module
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
@@ -19,6 +23,13 @@ app.use(express.json());
 // ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/todo", toDoRoutes);
+
+// Use the client app
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+);
 
 app.listen(PORT, () => {
   connection();
